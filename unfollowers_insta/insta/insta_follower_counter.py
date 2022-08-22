20150be2c07ee427db064a460f394df787c6f93d
unknown <juniomiguelsizeg@gmail.com>
2022-08-22 14:16:55 -0300

    'unfollowers_v1'

---
+++ unfollowers_insta/insta/insta_follower_counter.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+
+driver = webdriver.Firefox()
+
+page = driver.get('https://www.instagram.com')
+
+sleep(90)
+
+username_textfield = driver.find_element_by_name('username')
+
+password = driver.find_element_by_name('password')
+
+login_button = driver.find_element_by_class_name('sqdOP  L3NKy   y3zKF     ')
+
+username_textfield.click()
+username_textfield.sendkeys('juniomiguelsizeg@gmail.com')
+sleep(5)
+
+password.click()
+password.sendkeys('mj104598')
+
+sleep(5)
+
+login.button.click()
---
+++ unfollowers_insta/insta/unfollowers.py
@@ -0,0 +1,885 @@
+
+
+
+followers = """Seguidores
+Foto do perfil de resinart0991
+resinart0991
+Resinart
+Foto do perfil de naelsonflorencio
+naelsonflorencio
+Naelson Florêncio
+Foto do perfil de sebo_pereira_
+sebo_pereira_
+Sebo Pereira
+Foto do perfil de engenheirokaioqueiroz
+engenheirokaioqueiroz
+Foto do perfil de anna_mary9602
+anna_mary9602
+ANA MARIA
+Foto do perfil de flutterwithosama
+flutterwithosama
+Osama | Flutter | Mobile App development
+Foto do perfil de profissional_qualificado_br
+profissional_qualificado_br
+Profissional Qualificado
+Foto do perfil de automacao84
+automacao84
+#automacao
+Foto do perfil de vivianribeiroingles
+vivianribeiroingles
+Vivian Ribeiro
+Foto do perfil de anapaula_poderosa
+anapaula_poderosa
+Ana Paula Augusta
+Foto do perfil de milly_ittz
+milly_ittz
+Emilly Vitória LF
+Foto do perfil de mgalves21
+mgalves21
+Genoveva Alves
+Foto do perfil de flavioalves381
+flavioalves381
+Flávio alves
+Foto do perfil de gamer_rutz
+gamer_rutz
+Gamer_rutz
+Foto do perfil de xoliqov_muxriddin95
+xoliqov_muxriddin95
+Muxriddin Xoliqov
+Foto do perfil de homem_feliz_na_cama_2.0
+homem_feliz_na_cama_2.0
+Homem feliz na cama
+Foto do perfil de itachi_uchiha17.18
+itachi_uchiha17.18
+★🇹🇷ł₮₳₵Ⱨł ł₮₳₵Ⱨł🇹🇷★♚ρ'𝑗ℓ♚ @bonde_pjl
+Foto do perfil de sillva5975
+sillva5975
+Leia Sillva
+Foto do perfil de codingpowe.r
+codingpowe.r
+Codingpower
+Foto do perfil de memes.maromba.l
+memes.maromba.l
+·
+MEMES MAROMBA 
+Foto do perfil de natalieportman8l
+natalieportman8l
+·
+Natalie Portman
+Foto do perfil de conduta_de_honra
+conduta_de_honra
+Conduta De Honra
+Foto do perfil de millena.kivian
+millena.kivian
+Millena Kivian
+Foto do perfil de segue_a_bio_766890
+segue_a_bio_766890
+·
+Foto do perfil de 10mktgroup
+10mktgroup
+·
+10 MKT GROUP - Seu sucesso começa aqui
+Foto do perfil de praca.pizzariada
+praca.pizzariada
+pizzaria Da praça
+Foto do perfil de leonardo_leehmann
+leonardo_leehmann
+·
+investimentos.com/ROLETA DA VIRADA
+Foto do perfil de oficialthiagoolima
+oficialthiagoolima
+·
+Thiagoo Lima | DRT
+Foto do perfil de eulucassantos_17
+eulucassantos_17
+Lucas Santos
+Foto do perfil de _anacslima
+_anacslima
+Ana Lima
+Foto do perfil de codandojuntos_oficial
+codandojuntos_oficial
+Codando Juntos
+Foto do perfil de get_dating_courses
+get_dating_courses
+·
+Dating/RSD Courses
+Foto do perfil de morgana_blayer
+morgana_blayer
+Foto do perfil de francis_fxn_46
+francis_fxn_46
+Francis
+Foto do perfil de amaro_graziely
+amaro_graziely
+·
+Graziely Amaro
+Foto do perfil de automation.report
+automation.report
+The Automation Report
+Foto do perfil de treinovenom
+treinovenom
+TreinoVenom
+Foto do perfil de geici_limaq
+geici_limaq
+Geicilane Lima
+Foto do perfil de engenheiro_lucas
+engenheiro_lucas
+ENG° LUCAS HENRIQUE
+Foto do perfil de chad_vingativo
+chad_vingativo
+Chad Vingativo
+Foto do perfil de masterpacotefullstack
+masterpacotefullstack
+Full stack Master / Danki Code
+Foto do perfil de cortesdocodeftw
+cortesdocodeftw
+Cortes do CodeFTW
+Foto do perfil de joarteebeleza
+joarteebeleza
+Maria Joacilda
+Foto do perfil de eng.fabiokikuchi
+eng.fabiokikuchi
+eng.fabiokikuchi
+Foto do perfil de nicolasremboski
+nicolasremboski
+·
+Nicolas Remboski (NoFap)
+Foto do perfil de safad_inha34
+safad_inha34
+Foto do perfil de pamelaregina183
+pamelaregina183
+Pâmela Regina
+Foto do perfil de dexxter.gwm
+dexxter.gwm
+Foto do perfil de moraeslarissaramos
+moraeslarissaramos
+Larissa Helena Ramos Moraes
+Foto do perfil de fittnessmotiivation
+fittnessmotiivation
+F I T N E S M O T I V E N
+Foto do perfil de alavancagem_roleta_bet
+alavancagem_roleta_bet
+·
+Alavancagemroletabet
+Foto do perfil de maura_ago
+maura_ago
+sandra maura 
+Foto do perfil de dianagfelipe210506
+dianagfelipe210506
+Diana Gomes
+Foto do perfil de iemariadavilla
+iemariadavilla
+Maria Davilla
+Foto do perfil de prof.maciso
+prof.maciso
+Prof Michael Cisotto
+Foto do perfil de sumaeembalo
+sumaeembalo
+Sumaé Embaló
+Foto do perfil de profcarlosbarros
+profcarlosbarros
+Carlos Barros
+Foto do perfil de laim_construcoes
+laim_construcoes
+Laim C: gerais e metalúrgica
+Foto do perfil de vadimir_vava
+vadimir_vava
+Dimir Zeferino Cuma
+Foto do perfil de becomingthedivinevessel
+becomingthedivinevessel
+Don Gies
+Foto do perfil de vaipeengenharia
+vaipeengenharia
+Vaipe Engenharia
+Foto do perfil de jhenniferlsr
+jhenniferlsr
+jhennifer Rodrigues
+Foto do perfil de kingcolhere
+kingcolhere
+King col
+Foto do perfil de mauricio.barbosa16
+mauricio.barbosa16
+Mauricio Barbosa
+Foto do perfil de vivi_sousa523
+vivi_sousa523
+Viviane
+Foto do perfil de marciarobertaff
+marciarobertaff
+Marcia Roberta Falcão de Farias
+Foto do perfil de carlosbarros.churchman
+carlosbarros.churchman
+Carlos Barros
+Foto do perfil de itsoli_jana
+itsoli_jana
+Janaina Oliveira
+Foto do perfil de u.gomes_33
+u.gomes_33
+GOMES, Uitelo
+Foto do perfil de daygamepodcast
+daygamepodcast
+Coach Simon
+Foto do perfil de xande205martins
+xande205martins
+Alexandre Martins
+Foto do perfil de br_carvalho.eng
+br_carvalho.eng
+Eng° Civil Bruno Carvalho
+Foto do perfil de eu.filipe.masini
+eu.filipe.masini
+Filipe Santos
+Foto do perfil de sabiumsistemas
+sabiumsistemas
+Sabium Sistemas
+Foto do perfil de rose_copeland_ab
+rose_copeland_ab
+Foto do perfil de produtosflashoficial
+produtosflashoficial
+Flash Produtos 
+Foto do perfil de jocasta_marques
+jocasta_marques
+Jocasta Marques
+Foto do perfil de pensamento.alfa
+pensamento.alfa
+Lúcio Gomes
+Foto do perfil de escrotoosferas
+escrotoosferas
+EscrotoosFeras
+Foto do perfil de barbaraguandalin
+barbaraguandalin
+Barbara Guandalin
+Foto do perfil de brayan.cell_
+brayan.cell_
+·
+Brayan Cell
+Foto do perfil de cleysllabyannca123
+cleysllabyannca123
+Cleyslla Byannca Moraes
+Foto do perfil de taniasousa44
+taniasousa44
+Tania Sousa
+Foto do perfil de lkcriacoes_
+lkcriacoes_
+LK Criações
+Foto do perfil de _ni.co_las_
+_ni.co_las_
+·
+Nicolas
+Foto do perfil de valdiziaalmeida94
+valdiziaalmeida94
+·
+Valdizia Silva Almeida
+Foto do perfil de ana581carla
+ana581carla
+Ana Carla
+Foto do perfil de inventormilionario
+inventormilionario
+·
+Empreendedorismo & Motivação
+Foto do perfil de jociely__oliveira
+jociely__oliveira
+Jociely Oliveira
+Foto do perfil de gui_maschion
+gui_maschion
+Gui
+Foto do perfil de rosesantos4336
+rosesantos4336
+·
+Rose Santos
+Foto do perfil de eleide.moreira
+eleide.moreira
+Eleide Moreira
+Foto do perfil de confeitariamaisrealeza
+confeitariamaisrealeza
+·
+ConfeitariaMaisRealeza - Bolos
+Foto do perfil de hackerethicall
+hackerethicall
+·
+Winston Barros
+Foto do perfil de _sophia_laurene_
+_sophia_laurene_
+Sophia
+Foto do perfil de timothy.laviola
+timothy.laviola
+Timothy Laviola
+Foto do perfil de otechnomancer
+otechnomancer
+·
+O Technomancer
+Foto do perfil de edianasantiago10
+edianasantiago10
+·
+Ediana Santiago
+Foto do perfil de lab_prog_br
+lab_prog_br
+Laboratório de programação BR
+Foto do perfil de smart_program742
+smart_program742
+·
+smart program
+Foto do perfil de jackson48368
+jackson48368
+·
+Tee Caleb
+Foto do perfil de leilinha_mg
+leilinha_mg
+·
+Silva Leila
+Foto do perfil de abdolmehdi30
+abdolmehdi30
+·
+abdolmehdi
+Foto do perfil de vocedeveloper
+vocedeveloper
+Você Dev | Programação
+Foto do perfil de meccursosdigitais
+meccursosdigitais
+·
+meccursosdigitais
+Foto do perfil de isac.olive17
+isac.olive17
+Isac Oliveira
+Foto do perfil de androiddictionary
+androiddictionary
+·
+Foto do perfil de mundodoceda_gleicy
+mundodoceda_gleicy
+gleicy cakes
+Foto do perfil de mariah.oficiaal
+mariah.oficiaal
+·
+MARIAH.OFICIAAL
+Foto do perfil de _carloslima_5
+_carloslima_5
+carlos lima
+Foto do perfil de 7tododia.praxis
+7tododia.praxis
+·
+Todo dia nasce um anjo
+Foto do perfil de tecaqui.oficial
+tecaqui.oficial
+·
+TecAqui
+Foto do perfil de masterdesignsobrancelha
+masterdesignsobrancelha
+·
+Design de Sobrancelhas
+Foto do perfil de vozedados
+vozedados
+·
+Voz e Dados
+Foto do perfil de kate.spring.ig
+kate.spring.ig
+ATTRACTION & DATING COACH
+Foto do perfil de devlsadvocit
+devlsadvocit
+Jerome
+Foto do perfil de pamela_2802silva
+pamela_2802silva
+pamela_silva28
+Foto do perfil de laryh_lymah21
+laryh_lymah21
+·
+Läry'h Lymä'h
+Foto do perfil de uzuario_paralelo
+uzuario_paralelo
+·
+Eu_aleatório
+Foto do perfil de mrmarques2021
+mrmarques2021
+·
+Rodrigues
+Foto do perfil de mundo_da_informatica10
+mundo_da_informatica10
+·
+Mundo da Informatica
+Foto do perfil de patricia040286silva
+patricia040286silva
+Patricia Cristina
+Foto do perfil de damillyofficial
+damillyofficial
+·
+Damilly 
+Foto do perfil de rose.ane936
+rose.ane936
+·
+rosy
+Foto do perfil de virtudealfa
+virtudealfa
+·
+Virtude Alfa
+Foto do perfil de zaryluciasantiago
+zaryluciasantiago
+·
+Zarylucia Santiago
+Foto do perfil de deiammarques
+deiammarques
+·
+Andréa M Marques
+Foto do perfil de milionariodesperto
+milionariodesperto
+·
+Vida Plena/Mente/Dinheiro 
+Foto do perfil de canalalphaman
+canalalphaman
+AlphaMan
+Foto do perfil de blundaantonino
+blundaantonino
+Antonino Blunda
+Foto do perfil de zyrlanda
+zyrlanda
+Zyrlanda Silva
+Foto do perfil de daianealves42
+daianealves42
+Daiane Alves
+Foto do perfil de felipe_festas04
+felipe_festas04
+·
+felipe_festas04
+Foto do perfil de lamadamemarry
+lamadamemarry
+·
+Rodvieira
+Foto do perfil de dene_filha_de_deus
+dene_filha_de_deus
+·
+Dene Silva
+Foto do perfil de italo_oficial23
+italo_oficial23
+Italo Jaime
+Foto do perfil de uma_aprendiz22
+uma_aprendiz22
+g.l.e.i.c.y
+Foto do perfil de geslanne_santos_
+geslanne_santos_
+·
+Geslanne Alves
+Foto do perfil de lyyh_oficial_mp
+lyyh_oficial_mp
+Lyzyh Ysïïs Mätiiäs
+Foto do perfil de gleidianeggf
+gleidianeggf
+gleidianegomesfarias
+Foto do perfil de perfumaria_n_l
+perfumaria_n_l
+·
+𝒫𝑒𝓇𝒻𝓊𝓂𝒶𝓇𝒾𝒶 ♡
+Foto do perfil de enricodarocha
+enricodarocha
+Enrico darocha
+Foto do perfil de vandin002
+vandin002
+Erisvando Rodrigues
+Foto do perfil de pastoralemaoapolo
+pastoralemaoapolo
+Apolo
+Foto do perfil de eupatricia_cristina
+eupatricia_cristina
+Patricia Cristina
+Foto do perfil de ruyocosta
+ruyocosta
+Ruy Oliveira
+Foto do perfil de rosaocostaa
+rosaocostaa
+·
+Rosinha 
+Foto do perfil de lethycya_vitoria
+lethycya_vitoria
+Letícia Vitória
+Foto do perfil de josejoao_031
+josejoao_031
+·
+José João
+Foto do perfil de dr.pa16
+dr.pa16
+·
+Pedro Augusto
+Foto do perfil de ildisribeiro
+ildisribeiro
+Foto do perfil de tbts_da_tatah
+tbts_da_tatah
+Tatah
+Foto do perfil de casall_instaa
+casall_instaa
+G&V
+Foto do perfil de frases_todo_diia
+frases_todo_diia
+lembrete de frases
+Foto do perfil de filosofando2277
+filosofando2277
+Foto do perfil de lunatorres666
+lunatorres666
+Luna Torres
+Foto do perfil de hcstore_2
+hcstore_2
+HCSTORE
+Foto do perfil de tayson_youtuber
+tayson_youtuber
+TAYSON YOUTUBER
+Foto do perfil de keke25sousinha
+keke25sousinha
+Prazer, Keke
+Foto do perfil de brasil.dark
+brasil.dark
+Foto do perfil de marcela_cyntia_rosa
+marcela_cyntia_rosa
+Marcela Cyntia Rosa
+Foto do perfil de irisv_aldo
+irisv_aldo
+Foto do perfil de yagojose2018
+yagojose2018
+yagojose
+Foto do perfil de nutrilorenapaiva
+nutrilorenapaiva
+Lorena Paiva - Nutricionista 
+Foto do perfil de todabellaestilo
+todabellaestilo
+TodaBella_Estilo
+Foto do perfil de pedacinho.de_pano
+pedacinho.de_pano
+Pedacinho de Pano
+Foto do perfil de hey_kezia25
+hey_kezia25
+·
+kezia vitoria
+Foto do perfil de __mae__da__dalylla__
+__mae__da__dalylla__
+Dalylla 
+Foto do perfil de shaketimehebnnamichelly
+shaketimehebnnamichelly
+·
+Shake Time Hébnna Michelly
+Foto do perfil de thaismoreirapersonal
+thaismoreirapersonal
+·
+Thaís Moreira Personal Saúde
+Foto do perfil de elenir.costa.359
+elenir.costa.359
+Elenir Costa
+Foto do perfil de valeriasantosfreire
+valeriasantosfreire
+·
+Valeria Santos
+Foto do perfil de _rosados_ventos
+_rosados_ventos
+Rosa dos ventos 
+Foto do perfil de michellylimadesigner
+michellylimadesigner
+Michelly Designer Sobrancelhas
+Foto do perfil de mar_ia4099
+mar_ia4099
+Foto do perfil de jaimeazevedo23
+jaimeazevedo23
+Azevedo
+Foto do perfil de claudia_s.z
+claudia_s.z
+Claudia Sousa
+Foto do perfil de familialindaci1504
+familialindaci1504
+lindaci
+Foto do perfil de alineboutique22
+alineboutique22
+·
+Aline Boutique
+Foto do perfil de instadafamilya
+instadafamilya
+sigo de volta
+Foto do perfil de enzocoelho10
+enzocoelho10
+·
+Enzo coelho da rocha
+Foto do perfil de gut_0104
+gut_0104
+Gustavo 
+Foto do perfil de mfmodas20199
+mfmodas20199
+Mf Modas
+Foto do perfil de enricocoelhoda
+enricocoelhoda
+Enrico coelho da rocha
+Foto do perfil de coelhoitalo8
+coelhoitalo8
+Coelho Italo
+Foto do perfil de i.silva_2309
+i.silva_2309
+oliver
+Foto do perfil de armygleicy22
+armygleicy22
+jimin1310
+Foto do perfil de silva_agostinhoo
+silva_agostinhoo
+Rayssa Agostinho
+Foto do perfil de evanildoc477
+evanildoc477
+Evanildo Cavalcante
+Foto do perfil de amandakely.654
+amandakely.654
+Amanda kely
+Foto do perfil de angelica.mainguy
+angelica.mainguy
+angelica mainguy
+Foto do perfil de gilberlaniacoelho
+gilberlaniacoelho
+gilberlania coelho
+Foto do perfil de efraimcell.ce
+efraimcell.ce
+Efraim Cell e Computadores
+Foto do perfil de paloma_victoria15
+paloma_victoria15
+by paloma 
+Foto do perfil de eug_silvaaah
+eug_silvaaah
+familiasilvaeoliveira
+Foto do perfil de alinenunes20200
+alinenunes20200
+Aline Nunes
+Foto do perfil de topfitness873
+topfitness873
+TopFitness
+Foto do perfil de salaoespacogleice
+salaoespacogleice
+Salão Espaço Gleice
+Foto do perfil de ane_silva1984
+ane_silva1984
+gleicianesilva
+Foto do perfil de kamilafreitas2018
+kamilafreitas2018
+KAMILA
+Foto do perfil de erivam.oliveira.710
+erivam.oliveira.710
+Erivam Oliveira
+Foto do perfil de instadafamilia9
+instadafamilia9
+instadafamilia
+Foto do perfil de amiltonferreiraqueirosgmail.c
+amiltonferreiraqueirosgmail.c
+amilton_queiros111
+Foto do perfil de elisabethlima8008
+elisabethlima8008
+Elisabeth Lima
+Foto do perfil de _lidiana_freitas_
+_lidiana_freitas_
+Lidiana Oliveira
+Foto do perfil de pamela_regina78
+pamela_regina78
+Pâmela Regina
+Foto do perfil de cristina_sousa.2
+cristina_sousa.2
+Cristina Sousa
+Foto do perfil de guilherme.batatinha.10
+guilherme.batatinha.10
+Guilherme Batatinha
+Foto do perfil de tfcloset2
+tfcloset2
+TF closet
+Foto do perfil de eojeffyn_of
+eojeffyn_of
+·
+DIRETAMENTE DA FAVELA ORIGINAL
+Foto do perfil de yagojose2001
+yagojose2001
+yago2001
+Foto do perfil de rafaellagonzagada
+rafaellagonzagada
+Rafaella Gonzaga Da Costa
+Foto do perfil de luiscarloslimavieira
+luiscarloslimavieira
+Luis Carlos Lima Vieira
+Foto do perfil de cntshirt
+cntshirt
+·
+Cn T-shirt
+Foto do perfil de djesus0405
+djesus0405
+Maria De Jesus Oliveira
+Foto do perfil de pizzaria_aws
+pizzaria_aws
+Pizzaria_AWS Oficial
+Foto do perfil de confeitariadajul
+confeitariadajul
+Confeitaria da Ju
+Foto do perfil de fatima_l.santos
+fatima_l.santos
+·
+FÁTIMA(@Fatinha)❤DEUS MEU TUDO
+Foto do perfil de jose__junynhor_salles__10
+jose__junynhor_salles__10
+·
+Júnyor Salles
+Foto do perfil de magrinha45
+magrinha45
+magrecimento
+Foto do perfil de willamy002
+willamy002
+willamy
+Foto do perfil de florcakes__
+florcakes__
+Foto do perfil de wdistribuidora_
+wdistribuidora_
+O Melhor Doce Do Brasil
+Foto do perfil de ateliedacilene_
+ateliedacilene_
+·
+Ateliê da Cilene
+Foto do perfil de rodiny2019
+rodiny2019
+Rodiny19
+Foto do perfil de r8multimarcasss
+r8multimarcasss
+R8 Multimarcas
+Foto do perfil de _imperiodacoxinha
+_imperiodacoxinha
+Claudia Lima
+Foto do perfil de maura_coelhoo
+maura_coelhoo
+
+Foto do perfil de evandro3193
+evandro3193
+Evandro Oliveira
+Foto do perfil de italo.jaime.7
+italo.jaime.7
+Italo Olive
+Foto do perfil de francilenesouza000
+francilenesouza000
+francilenesouza00055522
+Foto do perfil de alessandro_fferreiraa
+alessandro_fferreiraa
+Alessandro Ferreira
+Foto do perfil de devanya_freitas_
+devanya_freitas_
+Devanya Freitas
+Foto do perfil de oliver_silva_22
+oliver_silva_22
+Foto do perfil de henriquebrian29
+henriquebrian29
+Henrique2309silva
+Foto do perfil de dayane_s2_chaves
+dayane_s2_chaves
+Dᥲᥡᥲᥒᥱ
+Foto do perfil de gleiciane_agostinho
+gleiciane_agostinho
+g.a.oliver
+Foto do perfil de uma_nova_mulher18
+uma_nova_mulher18
+gleiciany37
+Foto do perfil de carlosriconbr
+carlosriconbr
+Fotógrafo Carlos Ricon
+Foto do perfil de gilberlania_coelho
+gilberlania_coelho
+gilsouza
+Foto do perfil de gleyvando
+gleyvando
+casal Oliveira 
+Foto do perfil de franciscode485
+franciscode485
+Francisco de Assis Ferreira Jr
+Foto do perfil de muniropedroca
+muniropedroca
+Muniro Pedro Cá
+Foto do perfil de gabriel_2202oliveira
+gabriel_2202oliveira
+Pedro Gabriel
+Foto do perfil de iuliiakuznetsova2121
+iuliiakuznetsova2121
+Юлия Кузнецова
+Foto do perfil de rstm2796
+rstm2796
+·
+Foto do perfil de marialyima4321
+marialyima4321
+Maria Lima
+Foto do perfil de kalellviajante2018
+kalellviajante2018
+kalelljunior
+Foto do perfil de ra.fael7670
+ra.fael7670
+Rafael
+Foto do perfil de abdol_sardari
+abdol_sardari
+ab.sa
+Foto do perfil de micaeil1390
+micaeil1390
+, ABADAN
+Foto do perfil de blackout_redencao
+blackout_redencao
+BlACKOUT 9 DE NOVEMBRO
+Foto do perfil de juliocesarferreira.lima
+juliocesarferreira.lima
+Júlio César Ferreira Lima
+Foto do perfil de milena_silva_lima86
+milena_silva_lima86
+Milena Silva
+Foto do perfil de sylwiasuszczyk
+sylwiasuszczyk
+Sylwia Suszczyk
+Foto do perfil de yosafetkristiantoro3
+yosafetkristiantoro3
+Yusa Fall Out
+Foto do perfil de aliasdy1233
+aliasdy1233
+·
+میلادابوایاد
+Foto do perfil de _marcela.pereiira
+_marcela.pereiira
+MARCELA SILVA 
+Foto do perfil de sandra_oficial10
+sandra_oficial10
+
+Foto do perfil de poetry.of.moonlight
+poetry.of.moonlight
+a m o r y // ✿
+Foto do perfil de monia.bj
+monia.bj
+Monia
+Foto do perfil de yasmina_martins
+yasmina_martins
+·
+Yasmina Lorrana
+Foto do perfil de laim_da_matalugica2000
+laim_da_matalugica2000
+Lani Mentalugica
+Foto do perfil de _.mariah_silva
+_.mariah_silva
+Maria Silva
+Foto do perfil de psidalvahonorato
+psidalvahonorato
+Dalva Honorato
+Foto do perfil de gleiciane_oficial22
+gleiciane_oficial22
+Gleiciany Oliveira
+Foto do perfil de luanabrit60
+luanabrit60
+lana_Mënëzës
+Foto do perfil de conta.excluida.leia.descricao
+conta.excluida.leia.descricao
+·
+siga @buket.oficial
+Foto do perfil de teamchelseaeze
+teamchelseaeze
+Team Chelsea Eze
+Foto do perfil de vitoriamaria7546
+vitoriamaria7546
+Vitoria Maria
+
+"""
+
+
+"""with open('follow.csv', 'w',newline='\n') as file:
+    for name in followers:
+        file.write(name)
+    if file.close():
+        print('gravado e arquivo fechado')
+
+
+if __name__=='__main__':
+    
+    try:
+        for line in followers:
+            print(line, end=' ')
+            if line == 'a u t o m a c a o 8 4':
+                print('Vitória e sua amiga')
+    except UnicodeError:
+        pass
+       """ 
+
+
